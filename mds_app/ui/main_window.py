a79b32e8c400976d1f575813d839a242767f6bf2
Gustavo Mendes <63437313+Gustavo-A-Mendes@users.noreply.github.com>
2026-01-26 16:28:37 -0300

    :test_tube: test(main_window/menubar): add menubar

--- mds_app/ui/main_window.py
+++ mds_app/ui/main_window.py
@@ -1,43 +1,88 @@
 import tkinter as tk
 from tkinter import ttk
 
-from data.dataset import Dataset
-from ui.toolbar import ToolBar
-from ui.control_panel import ControlPanel
-from ui.visualization_area import VisualizationArea
+from mds_app.data.dataset import Dataset
+from mds_app.ui.menubar import MenuBar
+from mds_app.ui.toolbar import ToolBar
+from mds_app.ui.control_panel import ControlPanel
+from mds_app.ui.visualization_area import VisualizationArea
 
 
 class MainWindow:
     def __init__(self, root):
         self.root = root
         self.root.title("Analisador MDS Educacional")
-        self.root.geometry("1200x800")
+        self.root.geometry("900x600")
 
         self._create_layout()
 
+    # create all main window layout:
     def _create_layout(self):
-        # Paned principal (horizontal)
+        # ----------------------------------------------------------------------
+        # creating widgets:
+        # ----------------------------------------------------------------------
+
+        # main PanedWindow (horizontal):
         self.main_paned = tk.PanedWindow(self.root, orient="horizontal")
 
-        # Painel de controle
-        self.control_panel = ControlPanel(self.main_paned)
-        self.main_paned.add(self.control_panel, minsize=100)
-        # self.main_paned.paneconfigure(self.control_panel, minsize=200)
+        # ----------------------------------------------------------------------
+        # initialing custom widgets:
+        # ----------------------------------------------------------------------
 
-        # Área de visualização
-        self.visualization_area = VisualizationArea(self.main_paned)
-        self.main_paned.add(self.visualization_area, minsize=100)
-
-        # Barra de ferramentas
+        # dataset:
         self.dataset = Dataset()
 
+        # visualization_area:
+        self.visualization_area = VisualizationArea(self.main_paned)
+
+        # control panel:
+        self.control_panel = ControlPanel(
+            self.main_paned,
+            self.dataset,
+            self.visualization_area.show_matrix
+        )
+
+        # menubar:
+        self.menubar = MenuBar(
+            self.root,
+            self.dataset,
+            self.visualization_area,
+            on_import=self.control_panel.refresh
+        )
+
+        # toolbar:
         self.toolbar = ToolBar(
             self.root,
             self.dataset,
-            self.visualization_area
+            self.visualization_area,
+            on_import=self.control_panel.refresh
         )
 
+        # ----------------------------------------------------------------------
+        # configure event binds:
+        # ----------------------------------------------------------------------
+        '''...'''
+
+        # ----------------------------------------------------------------------
+        # setting layout:
+        # ----------------------------------------------------------------------
+
+        # adding widgets in panedWindow:
+        self.main_paned.add(self.control_panel, minsize=100)
+        self.main_paned.add(self.visualization_area, minsize=100)
+
+        self.root.update_idletasks()
+        self.main_paned.sash_place(0, 200, 0)
+        # self.main_paned.configure(sashwidth=20)
+
+        self.root.configure(menu=self.menubar)  # adding menubar
 
         self.toolbar.pack(side="top", fill="x")
         self.toolbar.pack_propagate(0)
         self.main_paned.pack(fill="both", expand=True)
+
+        # ----------------------------------------------------------------------
+        # :
+        # ----------------------------------------------------------------------
+        # self.control_panel.refresh()
+
---
+++ mds_app/ui/menubar.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+
+import pandas as pd
+from mds_app.utils.csv_loader import *
+from mds_app.ui.import_dialog import ImportDialog
+from mds_app.data.participant import Participant
+
+from mds_app.ui.control_panel import ControlPanel
+from mds_app.utils.csv_loader import separate_df, load_excel, load_csv, detect_file_type
+
+
+class MenuBar(tk.Menu):
+    def __init__(self, parent, dataset, visualization_area, on_import):
+        super().__init__(parent)
+        self.parent = parent
+        self.dataset = dataset
+        self.visualization_area = visualization_area
+        self.on_import = on_import
+        #
+        self._create_widgets()
+        # self.configure(height=100)
+
+    # create the toolbar area:
+    def _create_widgets(self):
+        # ----------------------------------------------------------------------
+        # creating widgets:
+        # ----------------------------------------------------------------------
+
+        # submenu:
+        self.file_menu = tk.Menu(self, tearoff=False)
+        self.file_menu.add_command(label="Abrir", command=self.import_csv)
+        # self.file_menu.add_separator()
+        self.add_cascade(label="File", menu=self.file_menu)
+    #     # button
+    #     self.btn_import = ttk.Button(self, text="Importar Dados", command=self.import_csv)
+    #     self.btn_manual = ttk.Button(self, text="Inserir Manualmente")
+    #     self.btn_manage = ttk.Button(self, text="Gerenciar Dados")
+    #     self.btn_mds = ttk.Button(self, text="Análise MDS")
+    #
+    #     # ----------------------------------------------------------------------
+    #     # setting layout:
+    #     # ----------------------------------------------------------------------
+    #     self.btn_import.pack(side="left", padx=5)
+    #     self.btn_manual.pack(side="left", padx=5)
+    #     self.btn_manage.pack(side="left", padx=5)
+    #     self.btn_mds.pack(side="left", padx=5)
+    #
+    # toolbar methods:
+    def import_csv(self):
+        file_path = Path(filedialog.askopenfilename(
+            filetypes=[
+                ("Arquivos CSV", "*.csv"),
+                ("Arquivos Excel", "*.xlsx *.xls"),
+                ("Todos os arquivos", "*.*")
+            ]
+        ))
+
+        # import canceled
+        if not file_path.name:
+            return
+
+        try:
+            ext = file_path.suffix.lower()
+            if ext == ".csv":
+                df = load_csv(file_path)
+            elif ext == ".xlsx" or ext == ".xls":
+                df = load_excel(file_path)
+            else:
+                messagebox.showerror("Erro", "Tipo de arquivo não suportado")
+                return
+
+            # detects file type:
+            file_type = detect_file_type(df, ext)
+
+            participants_data, headers = separate_df(df, file_type, ext)
+
+            self.dataset.set_participants(participants_data)
+
+            self.dataset.set_headers(headers)
+
+            if not headers:
+                messagebox.showerror("Erro", "Nenhuma informação válida encontrada.")
+                return
+
+            def on_confirm(new_headers):
+                self.dataset.set_selected_headers(new_headers)
+
+                # show first participant
+                self.visualization_area.show_dataframe(self.dataset, index=0)
+
+                self.on_import()
+
+            dialog = ImportDialog(self.parent, headers, on_confirm)
+            self.wait_window(dialog)
+
+        except Exception as e:
+            messagebox.showerror("Erro ao importar CSV", str(e))
